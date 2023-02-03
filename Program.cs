dca6403cbcd8531d56e16e82c944158597a6286b
carissa-c <122126939+carissa-c@users.noreply.github.com>
2023-02-03 15:28:01 -0500

    was not able to upload this morning. Added Justins notes to the end

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Please enter a word you would like to reverse.");
+string response = Console.ReadLine().ToLower().ToLower();
+
+static string ReverseWord(string words)
+{
+    Stack<string> word = new Stack<string>();
+    {
+        for (int i = 0; i < words.Length; i++)
+        {
+            word.Push(words.Substring(i, 1));
+        }
+
+        foreach (string reversed = "")
+        {
+            word.Pop(reversed.Substring(0));
+        }
+    }
+    Console.WriteLine(ReverseWord(response));
+
+}
+
+
+
+
